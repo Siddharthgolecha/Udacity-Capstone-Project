09f6f9c610cd73eb60d2bebc8d0b94465470cac8
Siddharthgolecha <golechasiddharth@gmail.com>
2020-08-05 11:55:50 +0000

    Inital commit

---
+++ CarND-Capstone/download_model.sh
@@ -0,0 +1,1 @@
+wget 'https://onedrive.live.com/download?cid=66F8B26BEC4D3A0A&resid=66F8B26BEC4D3A0A%216744&authkey=ADfDRAaHAe3FwUE' --no-check-certificate -O ./ros/src/tl_detector/light_classification/resnet50_5.h5
